8a294db4666a3ee6d70f2d0d096d753201faadbd
ryanyang1102 <57996660+ryanyang1102@users.noreply.github.com>
2021-08-27 01:08:25 +0800

    set vue-cli3.0 add header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  // created() {
+  //   const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/products`;
+  //   this.$http.get(api).then((response) => {
+  //     console.log(response.data);
+  //   });
+  // },
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./assets/scss/all"; // all 後面的 scss 可省略
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <header class="bg-light py-4">
+      <nav class="navbar navbar-expand-lg">
+        <div class="container">
+          <a class="navbar-brand" href="#"><img src="../assets/image/logo.png" width="65" height="75" alt="logo"></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fas fa-bars fz-9 text-lightGray"></i>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav fz-4 letter-spacing-2">
+              <li class="nav-item me-2"><a class=" px-2" href="#">首頁</a></li>
+              <li class="me-2"><a class="px-2" href="#">日出而作</a></li>
+              <li class="me-2"><a class="px-2" href="#">にがり工法</a></li>
+              <li class="me-2"><a class="px-2" href="#">產品介紹</a></li>
+              <li class=""><a class="px-2" href="#">店舖情報</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home'
+}
+</script>
